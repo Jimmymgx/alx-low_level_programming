9fc8d68d1c725143efa9bb327c3a9f653789a2e3
Jimmymgx <mohamedgamal.01199@gmail.com>
2023-06-16 08:47:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
