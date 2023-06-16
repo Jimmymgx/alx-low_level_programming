31ccaa6e83a71c91dffeec00a3916635df2c4c64
Jimmymgx <mohamedgamal.01199@gmail.com>
2023-06-16 08:52:13 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase in reverse.
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
