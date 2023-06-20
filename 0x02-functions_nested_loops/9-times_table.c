db88054525ff5cd1498540aa97c1ce6395d699dd
Mohamed gamal <71405393+Jimmymgx@users.noreply.github.com>
2023-06-20 19:29:27 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(result + '0');
-			} else if (k >= 10)
+			} else if (result >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
